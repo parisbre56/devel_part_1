c30264cf5f9a4a47ecb6657a5c43df3a9c5bfc55
parisbre56 <parisbre56@yahoo.gr>
2018-10-26 16:40:08 +0300

    no reason to check for negative when using unsigned

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -24,12 +24,12 @@ HashTable::HashTable(const Relation& relation,
         pSum(new uint32_t[buckets] { }),
         orderedTuples(new const Tuple*[relation.getNumTuples()] { }) {
     ConsoleOutput consoleOutput("HashTable");
-    if (this->buckets <= 0) {
+    if (this->buckets == 0) {
         throw runtime_error("buckets must be positive [buckets="
                             + to_string(this->buckets)
                             + "]");
     }
-    if (numTuples <= 0) {
+    if (numTuples == 0) {
         throw runtime_error("numTuples must be positive [numTuples="
                             + to_string(numTuples)
                             + "]");
@@ -37,10 +37,10 @@ HashTable::HashTable(const Relation& relation,
 
     CO_IFDEBUG(consoleOutput,
                "Splitting "
-          + to_string(numTuples)
-          + " tuples to "
-          + to_string(this->buckets)
-          + " buckets");
+               + to_string(numTuples)
+               + " tuples to "
+               + to_string(this->buckets)
+               + " buckets");
 
     CO_IFDEBUG(consoleOutput, "Generating histogram");
     for (uint32_t i = 0; i < numTuples; ++i) {
@@ -54,15 +54,15 @@ HashTable::HashTable(const Relation& relation,
     }
     if (consoleOutput.getDebugEnabled()) {
         consoleOutput.debugOutput("Created histogram with "
-                                   + to_string(buckets)
-                                   + " buckets");
+                                  + to_string(buckets)
+                                  + " buckets");
 
         for (uint32_t i = 0; i < buckets; ++i) {
             consoleOutput.debugOutput("[bucket="
-                                       + to_string(i)
-                                       + ", size="
-                                       + to_string(histogram[i])
-                                       + "]");
+                                      + to_string(i)
+                                      + ", size="
+                                      + to_string(histogram[i])
+                                      + "]");
         }
     }
 
@@ -72,10 +72,10 @@ HashTable::HashTable(const Relation& relation,
         for (uint32_t i = 0; i < buckets; ++i) {
             CO_IFDEBUG(consoleOutput,
                        "[bucket="
-                  + to_string(i)
-                  + ", pSum="
-                  + to_string(prevSum)
-                  + "]");
+                       + to_string(i)
+                       + ", pSum="
+                       + to_string(prevSum)
+                       + "]");
             pSum[i] = prevSum;
             prevSum += histogram[i];
             histogram[i] = 0; //Just so that we don't create a new array, we reuse the histogram array
@@ -92,9 +92,9 @@ HashTable::HashTable(const Relation& relation,
         uint32_t currHash = this->hashFunction(buckets, toCheck.getPayload());
         CO_IFDEBUG(consoleOutput,
                    "Copying to bucket "
-              + to_string(currHash)
-              + " position "
-              + to_string(histogram[currHash]));
+                   + to_string(currHash)
+                   + " position "
+                   + to_string(histogram[currHash]));
         orderedTuples[pSum[currHash] + histogram[currHash]] = new Tuple(toCheck);
         histogram[currHash]++;
     }
@@ -132,7 +132,7 @@ uint32_t HashTable::getBuckets() const {
 }
 
 uint32_t HashTable::getTuplesInBucket(uint32_t bucket) const {
-    if (bucket < 0 || bucket >= buckets) {
+    if (bucket >= buckets) {
         throw runtime_error("bucket out of bounds [bucket="
                             + to_string(bucket)
                             + ", buckets="
@@ -144,7 +144,7 @@ uint32_t HashTable::getTuplesInBucket(uint32_t bucket) const {
 }
 
 const Tuple * const * const HashTable::getBucket(uint32_t bucket) const {
-    if (bucket < 0 || bucket >= buckets) {
+    if (bucket >= buckets) {
         throw runtime_error("bucket out of bounds [bucket="
                             + to_string(bucket)
                             + ", buckets="
@@ -156,14 +156,14 @@ const Tuple * const * const HashTable::getBucket(uint32_t bucket) const {
 }
 
 const Tuple& HashTable::getTuple(uint32_t bucket, uint32_t index) const {
-    if (bucket < 0 || bucket >= buckets) {
+    if (bucket >= buckets) {
         throw runtime_error("bucket out of bounds [bucket="
                             + to_string(bucket)
                             + ", buckets="
                             + to_string(buckets)
                             + "]");
     }
-    if (index < 0 || index >= histogram[bucket]) {
+    if (index >= histogram[bucket]) {
         throw runtime_error("bucket index out of bounds [bucket="
                             + to_string(bucket)
                             + ", index="
@@ -210,7 +210,7 @@ string HashTable::toString() const {
                    << ","
                    << j
                    << ">: "
-            << (orderedTuples[pos])->toString();
+                   << (orderedTuples[pos])->toString();
         }
     }
     retVal << "]]";
